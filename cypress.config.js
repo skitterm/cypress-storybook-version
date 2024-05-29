9e79a3a1ad64dcf96a3012692c18a5a371c52fd2
Steven Kitterman <steven.kitterman@gmail.com>
2024-05-29 11:23:15 -0700

    werkz

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "cypress";
+import { installPlugin } from '@chromatic-com/cypress'  
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      installPlugin(on, config);
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+// cypress/support/e2e.js
+import "@chromatic-com/cypress/support";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
